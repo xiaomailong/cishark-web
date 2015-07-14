f34e688a678f2bfa9c1c52fe49c1112324218ad0
lisency <zh-y-sh@qq.com>
2015-07-14 10:19:03 +0800

    list all session info into index module

--- log/Lib/Action/IndexAction.class.php
+++ log/Lib/Action/IndexAction.class.php
@@ -4,14 +4,33 @@
  *
  */
 class IndexAction extends CommonAction {
+    protected $db_session;
     /**
      * (non-PHPdoc)
      * @see NavAction::_initialize()
      */
     public function _initialize(){
         parent::_initialize();
+        $this->db_session = D('session');
     }
     public function index(){
+        $sessions = $this->db_session->field("
+            session_id,
+            parent_id,
+            FROM_UNIXTIME(start_tv_sec) as start_tv_sec,
+            start_tv_usec,
+            FROM_UNIXTIME(end_tv_sec) as end_tv_sec,
+            end_tv_usec,
+            ip,
+            series_state,
+            cpu_state
+            ")
+            ->order('session_id desc')
+            ->select();
+
+        #dump($sessions);
+        $this->assign("sessions",$sessions);
+
         $this->display();
     }
 }
